0e843e42766af53b6aa6ce3d59b374dbc8185bc1
Jan Bryński <brynski.jan@gmail.com>
2024-03-20 14:11:26 +0100

    favicon

--- components/about.tsx
+++ components/about.tsx
@@ -11,14 +11,14 @@ export default function About() {
   return (
     <motion.section
       ref={ref}
-      className="mb-28 max-w-[45rem] text-center leading-8 sm:mb-40 scroll-mt-28"
+      className="max-w-[45rem] text-center leading-8 scroll-mt-28"
       initial={{ opacity: 0, y: 100 }}
       animate={{ opacity: 1, y: 0 }}
       transition={{ delay: 0.175 }}
       id="about"
     >
       <SectionHeading>About me</SectionHeading>
-      <p className="mb-3">
+      <p className="">
         Fullstack Software Engineer with 2 years of commercial experience,
         passionate about building user-centric web experiences in the JavaScript
         ecosystem. Adept at collaborating in team environments to deliver both
